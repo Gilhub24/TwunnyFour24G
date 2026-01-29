18036fa748cee0461f1ce8a43960c3466481ba23
Gilhub24 <gilgh324@gmail.com>
2026-01-29 22:47:45 +0400

    Create Layout.js

---
+++ pages/🧱 components/Layout.js
@@ -0,0 +1,12 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default function Layout({ children }) {
+  return (
+    <div className="app-root">
+      <Header />
+      <main className="app-main">{children}</main>
+      <Footer />
+    </div>
+  );
+}
